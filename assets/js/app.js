9849f293f98aa38d5f4545bf293dd8718f71702b
Gabriel <gabo5612@gmail.com>
2024-07-16 10:49:29 -0400

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -401,20 +401,23 @@ jQuery(document).ready(function($) {
             let param = ".Pork"
             filter_from_param(param)
             $('.wil-dropdown #protein-dd li').parents('.wil-dropdown').find('span').text("Pork");
+            viewMore()
         } else if (urlParams.get('filter')=='beef'){
             let param = ".Beef"
             filter_from_param(param)
             $('.wil-dropdown #protein-dd li').parents('.wil-dropdown').find('span').text("Beef");
+            viewMore()
         }   else if (urlParams.get('filter')=='bacon') {
             let param = ".Bacon"
             filter_from_param(param)
             $('.wil-dropdown #protein-dd li').parents('.wil-dropdown').find('span').text("Bacon");
+            viewMore()
         } else if (urlParams.get('filter')=='lamb') {
             let param = ".Lamb"
             filter_from_param(param)
             $('.wil-dropdown #protein-dd li').parents('.wil-dropdown').find('span').text("Lamb");
+            viewMore()
         }
-    viewMore()
     }
     function filter_from_param(param) {
         let theFilterValue = param
