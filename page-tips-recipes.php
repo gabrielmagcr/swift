4a04d6db194b7607ada52ed42b3308bde3b4d80d
Gabriel <gabo5612@gmail.com>
2024-07-17 10:22:38 -0400

    test

--- page-tips-recipes.php
+++ page-tips-recipes.php
@@ -105,11 +105,11 @@
                     if ($type == 'recipe') {
 
             ?>
-                       
+                        <a href="<?php the_permalink(); ?>" class=pi-top-link>
                             <div class="product-item all <?= the_field('type'); ?> <?= $methods; ?> <?= $hidden; ?> col-lg-4 col-sm-6 col-xs-12 col-md-4">
                            
                                 <div class="pi-top">
-                                <a href="<?php the_permalink(); ?>" class=pi-top-link>
+                               
                                     <div class="pi-top--shim">
                                    
 
@@ -123,13 +123,13 @@
                                         <!-- /.title -->
                                     </div>
                                     <!-- /.title-wrap -->
-                                    </a>
+                                   
                                 </div>
                             
                                 <!-- /.pi-top -->
                             
                             </div>
-                    
+                        </a>
             <?php
                     } // End of the if
                 } // End of the while
