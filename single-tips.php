d968ea64fc617be625e7743f614093f0da644811
Gabriel <gabo5612@gmail.com>
2024-07-19 12:30:28 -0400

    single tips

--- single-tips.php
+++ single-tips.php
@@ -1,63 +1,29 @@
-<?php get_header();?>
 <script>var activeTip = <?php print get_the_ID(); ?>;</script>
-<section id="tips-recipes-wrap">
+<?php get_header(); ?>
+<section class="sm-prodhero">
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <h1>Great Meals To Gather Around</h1>
-                <p>Pick your protein and your cooking style:</p>
-                <div class="clearfix"></div>
-                <div class="recipe-filter-wrap">
-                    <div class="wil-dropdown">
-                        <div class="wil-select">
-                            <span>Protein</span>
-                            <i class="fa fa-chevron-down"></i>
-                        </div>
-                        <ul id="protein-dd" class="wil-dropdown-menu r-filter-group" data-filter-group="protein">
-                            <li data-filter=".all">All</li>
-                            <li data-filter=".Pork">Pork</li>
-                            <li data-filter=".Beef">Beef</li>
-                            <!-- <li data-filter=".Lamb">Lamb (coming soon)</li> -->
-                        </ul>
-                    </div>
-
-                    <div class="wil-dropdown">
-                        <div class="wil-select">
-                            <span>Cooking Style</span>
-                            <i class="fa fa-chevron-down"></i>
-                        </div>
-                        <ul id="cooking-style-dd" class="wil-dropdown-menu r-filter-group" data-filter-group="method">
-                            <li data-filter=".all">All</li>
-                            <li data-filter=".skillet">Skillet</li>
-                            <li data-filter=".oven">Oven</li>
-                            <li data-filter=".grill">Grill</li>
-                            <li data-filter=".smoker">Smoker</li>
-                            <li data-filter=".multicooker">Multicooker</li>
-                            <li data-filter=".sous">Sous Vide</li>
-                            <li data-filter=".fryer">Fryer</li>
-                        </ul>
-                    </div>
-
-                    <div class="recipe-search-wrap">
-                        <div class="main-search-form">
-                            <label>
-                                <input type="search" class="search-field quicksearch" id="tip-search" placeholder="Search">
-                            </label>
-                        </div>
-                    </div> 
-                    <!-- /.recipe-search-wrap -->
+            <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-xs-12 sm-prodhero--info">
+                    <div class="sm-prodhero--breadcrumb">
+                    <ol>
+                        <li><a href="/tips-recipes">Recipes</a></li>
+                        <li><?php the_title(); ?></li>
+                    </ol>
                 </div>
-                <!-- /.recipe-filter-wrap -->
+                <h1><?php the_title(); ?></h1>
+                <p><?= the_field('description'); ?></p>
+                <form class="sm-prodhero--wtb" action="/products" method="GET">
+                    <button class="btn btn-outline-red" type="submit">Find your Protein</button>
+                </form>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-xs-12 sm-prodhero--img">
+                <figure>
+                    <img src="<?= the_field("image"); ?>" alt="<?php the_title(); ?> in packaging">
+                </figure>
             </div>
         </div>
-        <!-- /.row -->
-        <div class="clearfix"></div>
-        <div id="tips-content-wrap" class="row grid">
-            <?php get_template_part('inc/tips', 'loop'); ?>
-        </div>
-        <!-- /#tips-content-wrap.row -->
     </div>
-    <!-- /.container -->
 </section>
+
 <!-- /#tips-recipes-wrap -->
 <?php get_footer();?>
