72976758ff7e8a567988f22acfbca1a36489a9d0
Gabriel <gabo5612@gmail.com>
2024-07-19 15:07:22 -0400

    src

--- single-tips.php
+++ single-tips.php
@@ -83,14 +83,14 @@
                 <h3><span><?php echo $number_of_ingredients; ?></span> INGREDIENTS</h3>
             </div>
             <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-xs-12 tips-time">
-                <img href='/wp-content/uploads/2024/07/Vector.svg'><h3><span><?php echo $preparation_time; ?></span><?php echo $amount_of_time ?></h3>
+                <img src='/wp-content/uploads/2024/07/Vector.svg'><h3><span><?php echo $preparation_time; ?></span><?php echo $amount_of_time ?></h3>
             </div>
             <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-xs-12 tips-servings">
-                <img href='/wp-content/uploads/2024/07/Group.svg'><h3><span><?php echo $servings; ?></span>SERVINGS</h3>
+                <img src='/wp-content/uploads/2024/07/Group.svg'><h3><span><?php echo $servings; ?></span>SERVINGS</h3>
             </div>
             <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-xs-12 tips-servings">
                 <!-- agregar todos los tipos de coccion -->
-                <img href='/wp-content/uploads/2024/07/oven.svg'>
+                <img src='/wp-content/uploads/2024/07/oven.svg'>
             </div>
         </div>
     </div>
