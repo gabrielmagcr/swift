43157e6862c935e96b57d95a9b281f25d5f7ebe2
Gabriel <gabo5612@gmail.com>
2024-07-19 13:16:02 -0400

    styles mader with

--- single-tips.php
+++ single-tips.php
@@ -1,10 +1,28 @@
-<script>var activeTip = <?php print get_the_ID(); ?>;</script>
+<script>
+    var activeTip = <?php print get_the_ID(); ?>;
+</script>
 <?php get_header(); ?>
+<style>
+    .made-with{
+        background: #EBEBEB;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    font-size: 20px;
+    }
+    .made-with img{
+        width: 50%;
+    }
+    .made-with-span{
+        background: #FFAA2B;
+    }
+</style>
 <section class="sm-prodhero">
     <div class="container">
         <div class="row">
             <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-xs-12 sm-prodhero--info">
-                    <div class="sm-prodhero--breadcrumb">
+                <div class="sm-prodhero--breadcrumb">
                     <ol>
                         <li><a href="/tips-recipes">Recipes</a></li>
                         <li><?php the_title(); ?></li>
@@ -20,25 +38,26 @@
                 <figure>
                     <img src="<?= the_field("image"); ?>" alt="<?php the_title(); ?> in packaging">
                 </figure>
-                <?php $made_with= get_field('made_with');
-                if($made_with): ?>          
-                <?php foreach( $made_with as $post ): 
-                setup_postdata($post); ?>
-                     <li>
-                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                        <?php 
-                            $product_image = get_field('product_image');
-                            if ($product_image): ?>
-                                <img src="<?= $product_image; ?>" alt="<?php the_title(); ?>"/>
-                            <?php endif; ?>
-                      </li>
-                <?php endforeach; ?>
-                <?php wp_reset_postdata(); ?>
-                    <?php endif; ?>
+                <?php $made_with = get_field('made_with');
+                if ($made_with) : ?>
+
+                    <span class="made-with-span">MADE WITH:</span>
+                    <?php foreach ($made_with as $post) :
+                        setup_postdata($post); ?>
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?><div class="made-with">
+                                <?php
+                                $product_image = get_field('product_image');
+                                if ($product_image) : ?>
+                                    <img src="<?= $product_image; ?>" alt="<?php the_title(); ?>" />
+                                <?php endif; ?>
+                            </div></a>
+                    <?php endforeach; ?>
+                    <?php wp_reset_postdata(); ?>
+                <?php endif; ?>
             </div>
         </div>
     </div>
 </section>
 
 <!-- /#tips-recipes-wrap -->
-<?php get_footer();?>
+<?php get_footer(); ?>
