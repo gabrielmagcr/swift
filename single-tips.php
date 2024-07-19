5a92052d1e3babccaa5dacb388c40b48b455b9a0
Gabriel <gabo5612@gmail.com>
2024-07-19 13:56:00 -0400

    better structure

--- single-tips.php
+++ single-tips.php
@@ -71,16 +71,22 @@
 </section>
 
 <section class="sm-badgebar">
+    <?php 
+        $number_of_ingredients= get_field('number_of_ingredients');
+        $preparation_time= get_field('preparation_time');
+        $amount_of_time= get_field('amount_of_time');
+        $servings=get_field('servings');
+    ?>
     <div class="container">
         <div class="row">
             <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-xs-12 tips-ingredients">
-                <h3><span><?php get_field('number_of_ingredients'); ?></span> INGREDIENTS</h3>
+                <h3><span><?php echo $number_of_ingredients; ?></span> INGREDIENTS</h3>
             </div>
             <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-xs-12 tips-time">
-                <img href='/wp-content/uploads/2024/07/Vector.svg'><h3><span><?php get_field('number_of_ingredients'); ?></span><?php get_field('amount_of_time');?></h3>
+                <img href='/wp-content/uploads/2024/07/Vector.svg'><h3><span><?php echo $preparation_time; ?></span><?php echo $amount_of_time ?></h3>
             </div>
             <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-xs-12 tips-servings">
-                <img href='/wp-content/uploads/2024/07/Group.svg'><h3><span><?php get_field('servings'); ?></span>SERVINGS</h3>
+                <img href='/wp-content/uploads/2024/07/Group.svg'><h3><span><?php echo $servings; ?></span>SERVINGS</h3>
             </div>
             <div class="col-xl-6 col-lg-6 col-md-6 col-12 col-xs-12 tips-servings">
                 <!-- agregar todos los tipos de coccion -->
