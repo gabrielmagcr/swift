965ee817e69a1ea4c9a49b9c5f7809f2e6b963d8
Gabriel <gabo5612@gmail.com>
2024-10-10 18:20:01 -0400

    src

--- page-heritage.php
+++ page-heritage.php
@@ -1,4 +1,7 @@
-<?php get_header(); ?>
+<?php get_header();
+
+$swiftImg = "/wp-content/themes/swiftMeatsv2/assets/img/heritage/Redesign-2024/SWIFT.svg"
+?>
 <section class="hero-heritage">
     <div class="h-h-our-story">
         <h1>Our Story</h1>
@@ -6,12 +9,12 @@
     </div>
     <div class="h-h-biography">
         <div class="h-h-biography-left">
-             <img src="/wp-content/themes/swiftMeatsv2/assets/img/heritage/Redesign-2024/Frame-214.webp" alt="Gustavus Swift">
+            <img src="/wp-content/themes/swiftMeatsv2/assets/img/heritage/Redesign-2024/Frame-214.webp" alt="Gustavus Swift">
         </div>
         <div class="h-h-biography-right">
             <img src="/wp-content/themes/swiftMeatsv2/assets/img/heritage/Redesign-2024/Old-Swift.webp" alt="Old Swift">
             <p>
-            Swift then branches out to Brighton, Massachusetts, Albany, and Buffalo, New York. His next stop is the windy city of Chicago, where he establishes business there in 1875.<br><br>Take a journey with us, and discover the timeline of events that made Swift the brand it is today.<br><br>See how we’ve been inspiring the extraordinary since 1855.
+                Swift then branches out to Brighton, Massachusetts, Albany, and Buffalo, New York. His next stop is the windy city of Chicago, where he establishes business there in 1875.<br><br>Take a journey with us, and discover the timeline of events that made Swift the brand it is today.<br><br>See how we’ve been inspiring the extraordinary since 1855.
             </p>
         </div>
     </div>
@@ -22,23 +25,20 @@
 </section>
 <section class="dates-badge">
     <div class="dates">
-        <?php 
-            $swiftImg="/wp-content/themes/swiftMeatsv2/assets/img/heritage/Redesign-2024/SWIFT.svg"
-        ?>
-        <div class="date date-active"><a href="#1855"><img href="<?php $swiftImg ?>"><span>1855</span></a></div>
-        <div class="date"><a href="#1861"><img href="<?php $swiftImg ?>"><span>1861</span></a></div>
-        <div class="date"><a href="#1862"><img href="<?php $swiftImg ?>"><span>1862</span></a></div>
-        <div class="date"><a href="#1872"><img href="<?php $swiftImg ?>"><span>1872</span></a></div>
-        <div class="date"><a href="#1875"><img href="<?php $swiftImg ?>"><span>1875</span></a></div>
-        <div class="date"><a href="#1878"><img href="<?php $swiftImg ?>"><span>1878</span></a></div>
-        <div class="date"><a href="#1880"><img href="<?php $swiftImg ?>"><span>1880</span></a></div>
-        <div class="date"><a href="#1893"><img href="<?php $swiftImg ?>"><span>1893</span></a></div>
-        <div class="date"><a href="#1900"><img href="<?php $swiftImg ?>"><span>1900</span></a></div>
-        <div class="date"><a href="#1903"><img href="<?php $swiftImg ?>"><span>1903</span></a></div>
-        <div class="date"><a href="#1915"><img href="<?php $swiftImg ?>"><span>1915</span></a></div>
-        <div class="date"><a href="1924"><img href="<?php $swiftImg ?>"><span>1924</span></a></div>
-        <div class="date"><a href="#1943"><img href="<?php $swiftImg ?>"><span>1943</span></a></div>
-        <div class="date"><a href="#1964"><img href="<?php $swiftImg ?>"><span>1964</span></a></div>
+        <div class="date date-active"><a href="#1855"><img src="<?php $swiftImg ?>"><span>1855</span></a></div>
+        <div class="date"><a href="#1861"><img src="<?php $swiftImg ?>"><span>1861</span></a></div>
+        <div class="date"><a href="#1862"><img src="<?php $swiftImg ?>"><span>1862</span></a></div>
+        <div class="date"><a href="#1872"><img src="<?php $swiftImg ?>"><span>1872</span></a></div>
+        <div class="date"><a href="#1875"><img src="<?php $swiftImg ?>"><span>1875</span></a></div>
+        <div class="date"><a href="#1878"><img src="<?php $swiftImg ?>"><span>1878</span></a></div>
+        <div class="date"><a href="#1880"><img src="<?php $swiftImg ?>"><span>1880</span></a></div>
+        <div class="date"><a href="#1893"><img src="<?php $swiftImg ?>"><span>1893</span></a></div>
+        <div class="date"><a href="#1900"><img src="<?php $swiftImg ?>"><span>1900</span></a></div>
+        <div class="date"><a href="#1903"><img src="<?php $swiftImg ?>"><span>1903</span></a></div>
+        <div class="date"><a href="#1915"><img src="<?php $swiftImg ?>"><span>1915</span></a></div>
+        <div class="date"><a href="1924"><img src="<?php $swiftImg ?>"><span>1924</span></a></div>
+        <div class="date"><a href="#1943"><img src="<?php $swiftImg ?>"><span>1943</span></a></div>
+        <div class="date"><a href="#1964"><img src="<?php $swiftImg ?>"><span>1964</span></a></div>
     </div>
 </section>
 <?php get_footer(); ?>
