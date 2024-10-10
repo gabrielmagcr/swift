727046cb23afe524503642650a8ecba233f3f860
Gabriel <gabo5612@gmail.com>
2024-10-10 15:33:05 -0400

    fix address

--- page-heritage.php
+++ page-heritage.php
@@ -6,17 +6,17 @@
     </div>
     <div>
         <div>
-             <img src="/wp-content/themes/swift/assets/img/heritage/Redesign-2024/Frame-214.webp" alt="Gustavus Swift">
+             <img src="/wp-content/themes/swiftMeatsv2/assets/img/heritage/redesign-2024/Frame-214.webp" alt="Gustavus Swift">
         </div>
         <div>
-            <img src="/wp-content/themes/swift/assets/img/heritage/Redesign-2024/Old-Swift.webp" alt="Old Swift">
+            <img src="/wp-content/themes/swiftMeatsv2/assets/img/heritage/redesign-2024/Old-Swift.webp" alt="Old Swift">
             <p>
             Swift then branches out to Brighton, Massachusetts, Albany, and Buffalo, New York. His next stop is the windy city of Chicago, where he establishes business there in 1875.<br>Take a journey with us, and discover the timeline of events that made Swift the brand it is today.<br>See how we’ve been inspiring the extraordinary since 1855.
             </p>
         </div>
     </div>
     <div>
-        <img src="/wp-content/themes/swift/assets/img/heritage/Redesign-2024/pointer-1.webp" alt="Pointer SwiftMeats">
+        <img src="/wp-content/themes/swiftMeatsv2/assets/img/heritage/redesign-2024/pointer-1.webp" alt="Pointer SwiftMeats">
         <span>Discover the timeline of events that made Swift the brand it is today.</span>
     </div>
 </section>
