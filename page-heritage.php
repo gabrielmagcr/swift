d644d64ad634cfa51fe0359a806bd177cce6f3f3
Gabriel <gabo5612@gmail.com>
2024-10-11 09:25:06 -0400

    styles 1855

--- page-heritage.php
+++ page-heritage.php
@@ -3,24 +3,26 @@
 $swiftImg = "/wp-content/themes/swiftMeatsv2/assets/img/heritage/Redesign-2024/OurStory/SWIFT.svg"
 ?>
 <section class="hero-heritage">
-    <div class="h-h-our-story">
-        <h1>Our Story</h1>
-        <p>At age 16, Gustavus Swift founds his butcher shop business in Eastham, Massachusetts. With help from his family, Swift’s father advances him $20 for a heifer, with which he makes a $10 profit. Later, his uncle lends him $400 to start his business in earnest.</p>
-    </div>
-    <div class="h-h-biography">
-        <div class="h-h-biography-left">
-            <img src="/wp-content/themes/swiftMeatsv2/assets/img/heritage/Redesign-2024/OurStory/Frame-214.webp" alt="Gustavus Swift">
+    <div class="container">
+        <div class="h-h-our-story">
+            <h1>Our Story</h1>
+            <p>At age 16, Gustavus Swift founds his butcher shop business in Eastham, Massachusetts. With help from his family, Swift’s father advances him $20 for a heifer, with which he makes a $10 profit. Later, his uncle lends him $400 to start his business in earnest.</p>
         </div>
-        <div class="h-h-biography-right">
-            <img src="/wp-content/themes/swiftMeatsv2/assets/img/heritage/Redesign-2024/OurStory/Old-Swift.webp" alt="Old Swift">
-            <p>
-                Swift then branches out to Brighton, Massachusetts, Albany, and Buffalo, New York. His next stop is the windy city of Chicago, where he establishes business there in 1875.<br><br>Take a journey with us, and discover the timeline of events that made Swift the brand it is today.<br><br>See how we’ve been inspiring the extraordinary since 1855.
-            </p>
+        <div class="h-h-biography">
+            <div class="h-h-biography-left">
+                <img src="/wp-content/themes/swiftMeatsv2/assets/img/heritage/Redesign-2024/OurStory/Frame-214.webp" alt="Gustavus Swift">
+            </div>
+            <div class="h-h-biography-right">
+                <img src="/wp-content/themes/swiftMeatsv2/assets/img/heritage/Redesign-2024/OurStory/Old-Swift.webp" alt="Old Swift">
+                <p>
+                    Swift then branches out to Brighton, Massachusetts, Albany, and Buffalo, New York. His next stop is the windy city of Chicago, where he establishes business there in 1875.<br><br>Take a journey with us, and discover the timeline of events that made Swift the brand it is today.<br><br>See how we’ve been inspiring the extraordinary since 1855.
+                </p>
+            </div>
         </div>
-    </div>
-    <div class="h-h-biography-end">
-        <img src="/wp-content/themes/swiftMeatsv2/assets/img/heritage/Redesign-2024/OurStory/pointer-1.webp" alt="Pointer SwiftMeats">
-        <span>Discover the timeline of events that made Swift the brand it is today.</span>
+        <div class="h-h-biography-end">
+            <img src="/wp-content/themes/swiftMeatsv2/assets/img/heritage/Redesign-2024/OurStory/pointer-1.webp" alt="Pointer SwiftMeats">
+            <span>Discover the timeline of events that made Swift the brand it is today.</span>
+        </div>
     </div>
 </section>
 <section class="dates-badge">
