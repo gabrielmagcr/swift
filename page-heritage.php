8fdd191d104554c8e316ee062febcde62e5660d4
Gabriel <gabo5612@gmail.com>
2024-10-14 13:35:48 -0400

    1915 structure

--- page-heritage.php
+++ page-heritage.php
@@ -201,4 +201,17 @@ $swiftImg = "/wp-content/themes/swiftMeatsv2/assets/img/heritage/Redesign-2024/O
 
     </div>
 </section>
+<section id="1915" class="year-1915">
+    <div class="container container-1915">
+        <div class="left-1915">    
+    <div class="h-text">
+            <h3>Safety above all</h3>
+            <p>Swift creates a ‘Safety First’ campaign to ensure the safety of its employees. This campaign cuts plant accidents in half.
+            </p>
+        </div></div>
+        <div class="right-1915 h-images">
+            <img src="/wp-content/themes/swiftMeatsv2/assets/img/heritage/Redesign-2024/1915-1/1915-1.png" alt=""><img src="/wp-content/themes/swiftMeatsv2/assets/img/heritage/Redesign-2024/1915-2/1915-2.png" alt="">
+        </div>
+    </div>
+</section>
 <?php get_footer(); ?>
