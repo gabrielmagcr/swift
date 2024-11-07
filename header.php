5d53890eda7833dd0a12b0a6270b8d79092cd6a9
Gabriel <gabo5612@gmail.com>
2024-11-07 10:16:56 -0400

    test

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <head>
 	<meta charset="utf-8">
 	<!-- Osano Cookie Consent -->
-	<script src=https://cmp.osano.com/169m5QToXBlu63VAe/9c1544c8-47fb-4f1c-9688-c9f7d54a48cb/osano.js></script>
+	<script async  src=https://cmp.osano.com/169m5QToXBlu63VAe/9c1544c8-47fb-4f1c-9688-c9f7d54a48cb/osano.js></script>
 	<!-- Force IE to use the latest rendering engine available -->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<!-- Mobile Meta -->
@@ -26,18 +26,18 @@
 	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" type="image/x-icon">
 	<?php } ?>
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-	<script src="https://kit.fontawesome.com/ee01b20d7e.js" crossorigin="anonymous"></script>
+	<script async src="https://kit.fontawesome.com/ee01b20d7e.js" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
 	<?php wp_head(); ?>
-	<script src="https://cdn.rawgit.com/nnattawat/flip/master/dist/jquery.flip.min.js"></script>
+	<script async src="https://cdn.rawgit.com/nnattawat/flip/master/dist/jquery.flip.min.js"></script>
 	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	<script async>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 	})(window,document,'script','dataLayer','GTM-PWNBCJJ');</script>
 	<!-- End Google Tag Manager -->
-	<script type="text/javascript">new Image(1,1).src="//ad.ipredictive.com/d/track/cvt/pixel?acct_id=53617&cache_buster="+Math.floor(Date.now()/1000);</script>
+	<script async  type="text/javascript">new Image(1,1).src="//ad.ipredictive.com/d/track/cvt/pixel?acct_id=53617&cache_buster="+Math.floor(Date.now()/1000);</script>
 	
 	<noscript>
 	<img src="//ad.ipredictive.com/d/track/cvt/pixel?acct_id=53617&cache_buster=[timestamp]" height="1" width="1" style="display:none"></img>
