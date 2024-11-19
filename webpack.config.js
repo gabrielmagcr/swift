522d1c68bf8fe5c4f083ac1279944f0097daa594
Gabriel <gabo5612@gmail.com>
2024-11-19 13:36:10 -0400

    testing 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,42 @@
 process.traceDeprecation = true;
 const path = require('path');
 const webpack = require('webpack');
-const fs = require('fs');
-const ExtractCssChunks = require("extract-css-chunks-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WebpackBuildNotifierPlugin = require('webpack-build-notifier');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const MinifyPlugin = require("babel-minify-webpack-plugin");
-const ProgressBarPlugin = require('progress-bar-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const WriteFilePlugin = require('write-file-webpack-plugin');
-const DashboardPlugin = require('webpack-dashboard/plugin');
 const devMode = process.env.NODE_ENV !== 'production';
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const PROXY = 'https://swift.local';
 const PUBLIC_PATH = '/wp-content/themes/swift/';
 
 const config = {
-    mode: !devMode ? 'production' : 'development',
+    mode: devMode ? 'development' : 'production',
 
-    // Modificación: Definir múltiples entradas para cada página
+    // Entradas separadas para cada archivo de JS y SCSS
     entry: {
-        app: path.resolve(__dirname, './assets/js/app.js'), // Script principal
-        sweepstakes: path.resolve(__dirname, './assets/scss/sweepstakes.scss'),
-        contact: path.resolve(__dirname, './assets/scss/contact.scss'),
-        heritage: path.resolve(__dirname, './assets/scss/heritage.scss'),
-        veal: path.resolve(__dirname, './assets/scss/veal.scss'),
-        lowes: path.resolve(__dirname, './assets/scss/lowes.scss'),
-        masterclass: path.resolve(__dirname, './assets/scss/masterclass.scss'),
-        offers: path.resolve(__dirname, './assets/scss/offers.scss'),
-        privacy: path.resolve(__dirname, './assets/scss/privacy.scss'),
-        products: path.resolve(__dirname, './assets/scss/products.scss'),
-        locator: path.resolve(__dirname, './assets/scss/locator.scss'),
-        sustain: path.resolve(__dirname, './assets/scss/sustain.scss'),
-        tailgate_with_swift: path.resolve(__dirname, './assets/scss/tailgate_with_swift.scss'),
-        tips: path.resolve(__dirname, './assets/scss/tips.scss'),
-        search: path.resolve(__dirname, './assets/scss/search.scss'),
-        recipes: path.resolve(__dirname, './assets/scss/recipes.scss'),
-        product: path.resolve(__dirname, './assets/scss/single-products.scss'),
-    },
+        app: [path.resolve(__dirname, './assets/js/app.js'), path.resolve(__dirname, './assets/scss/app.scss')],  // Archivo global
+        heritage: [path.resolve(__dirname, './assets/js/heritage.js'), path.resolve(__dirname, './assets/scss/heritage.scss')],
+        masterclass: [path.resolve(__dirname, './assets/js/masterclass.js'), path.resolve(__dirname, './assets/scss/masterclass.scss')],
+        offers: [path.resolve(__dirname, './assets/js/offers.js'), path.resolve(__dirname, './assets/scss/offers.scss')],
+        contact: [path.resolve(__dirname, './assets/js/contact.js'), path.resolve(__dirname, './assets/scss/contact.scss')],
+        products: [path.resolve(__dirname, './assets/js/products.js'), path.resolve(__dirname, './assets/scss/products.scss')],
+        sweepstakes: [path.resolve(__dirname, './assets/js/sweepstakes.js'), path.resolve(__dirname, './assets/scss/sweepstakes.scss')],
+        privacy: [path.resolve(__dirname, './assets/js/privacy.js'), path.resolve(__dirname, './assets/scss/privacy.scss')],
+        sustain: [path.resolve(__dirname, './assets/js/sustain.js'), path.resolve(__dirname, './assets/scss/sustain.scss')],
+        locator: [path.resolve(__dirname, './assets/js/locator.js'), path.resolve(__dirname, './assets/scss/locator.scss')],
+        tips: [path.resolve(__dirname, './assets/js/tips.js'), path.resolve(__dirname, './assets/scss/tips.scss')],
+        search: [path.resolve(__dirname, './assets/js/search.js'), path.resolve(__dirname, './assets/scss/search.scss')],
+        recipes: [path.resolve(__dirname, './assets/js/recipes.js'), path.resolve(__dirname, './assets/scss/recipes.scss')],
+        product: [path.resolve(__dirname, './assets/js/product.js'), path.resolve(__dirname, './assets/scss/single-products.scss')],
+    },    
 
-    devServer: !devMode ? undefined : {
+    devServer: devMode ? {
         port: 9494,
         host: '0.0.0.0',
-        hot: true,
         https: true,
-        allowedHosts: [
-            '.swift.local',
-            '.bowtie-vagrant.test',
-        ],
+        allowedHosts: ['.swift.local', '.bowtie-vagrant.test'],
         headers: { 'Access-Control-Allow-Origin': '*' },
         contentBase: path.join(__dirname, '.'),
         watchContentBase: true,
@@ -66,76 +53,54 @@ const config = {
                 secure: false
             }
         }
-    },
+    } : undefined,
 
-    devtool: devMode ? 'inline-source-map' : undefined,
+    devtool: devMode ? 'inline-source-map' : false,
 
     output: {
-        filename: 'scripts/[name].js',  // Salida para scripts JS
+        filename: 'scripts/[name].js',  // Cada archivo JS va a dist/scripts/
         path: path.resolve(__dirname, './dist'),
         publicPath: PUBLIC_PATH
     },
 
-    resolve: {
-        extensions: ['.js', '.vue', '.json'],
-        alias: {
-            'vue$': 'vue/dist/vue.esm.js',
-            '@': path.resolve(__dirname, '.')
-        }
-    },
-
     module: {
-       rules: [
+        rules: [
             {
                 test: /\.vue$/,
                 loader: 'vue-loader',
             },
             {
-                test: /\.css$/,
+                test: /\.(sa|sc|c)ss$/,
                 use: [
-                    'css-hot-loader',
-                    ExtractCssChunks.loader,
-                    "css-loader"
-                ]
-            },
-           {
-               test: /\.(sa|sc|c)ss$/,
-               use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
                     {
-                        loader: ExtractCssChunks.loader,
+                        loader: 'postcss-loader',
                         options: {
-                            hot: true
+                            postcssOptions: {
+                                plugins: [require('autoprefixer')()]
+                            },
+                            sourceMap: devMode
                         }
                     },
-                   'css-loader',
-                   {
-                       loader: 'postcss-loader',
-                       options: {
-                           config: {
-                               path: path.resolve(__dirname, './postcss.config.js')
-                           }
-                       }
-                   },
-                   {
-                       loader: 'sass-loader',
-                       options: {
-                           // Importar variables globales o mixins si es necesario
-                       }
-                   },
-               ],
-           },
-           {
-            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-            use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]',
-                  outputPath: 'fonts/',
-                  publicPath: PUBLIC_PATH + 'dist/fonts/'
-                }
-              }
-            ]
+                    {
+                        loader: 'sass-loader',
+                        options: { sourceMap: devMode }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg|png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images/',
+                            publicPath: PUBLIC_PATH + 'dist/images/'
+                        }
+                    }
+                ]
             },
             {
                 test: /\.js$/,
@@ -148,31 +113,18 @@ const config = {
     plugins: [
         new CopyWebpackPlugin([]),
         new VueLoaderPlugin(),
-        new ExtractCssChunks({
-            hot: true,
-            filename: 'styles/[name].css', // Genera un archivo CSS por cada entrada
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].css'  // Genera un archivo CSS por cada entrada
         }),
         new WebpackBuildNotifierPlugin({
             sound: 'Funk',
             successSound: 'Pop'
         }),
         new WriteFilePlugin({
-            force: true,
-            test: /^(?!.*(hot)).*/,
-        }),
-    ],
+            force: true,  // Escribir los archivos en disco en modo desarrollo
+            test: /^(?!.*(hot)).*/,  // Evita escribir los archivos temporales de HMR
+        })
+    ]
 };
-
-// Configuración de plugins para producción
-if (process.env.NODE_ENV === 'production') {
-    config.plugins.push(
-        // new CleanWebpackPlugin(),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': '"production"'
-        }),
-        new MinifyPlugin(),
-        new OptimizeCssAssetsPlugin()  // Optimización y minificación de los CSS generados
-    );
-}
 
 module.exports = config;
