2d2185a20065333d51cdf8f22e34b64635aa4cda
Gabriel <gabo5612@gmail.com>
2024-11-20 12:43:09 -0400

    test

--- assets/js/tips.js
+++ assets/js/tips.js
@@ -1,4 +1,46 @@
-import './_dropdown';
+$('.wil-dropdown').click(function () {
+    $(this).attr('tabindex', 1).focus();
+    $(this).toggleClass('active');
+    $(this).find('.wil-dropdown-menu').slideToggle(300);
+});
+$('.wil-dropdown').focusout(function () {
+    $(this).removeClass('active');
+    $(this).find('.wil-dropdown-menu').slideUp(300);
+});
+$('.wil-dropdown .wil-dropdown-menu li').click(function () {
+    $(this).parents('.wil-dropdown').find('span').text($(this).text());
+    $(this).parents('.wil-dropdown').find('').text($(this).text());
+    viewMore();
+    // $(this).parents('.wil-dropdown').find('input').attr('value', $(this).attr('id'));
+});
+/*End Dropdown Menu*/
+
+let urlParams = new URLSearchParams(window.location.search)
+if(urlParams.has('filter')) {
+    if(urlParams.get('filter') == 'pork') {
+        let param = ".Pork"
+        filter_from_param(param)
+        $('.wil-dropdown #protein-dd li').parents('.wil-dropdown').find('span').text("Pork");
+    } else if (urlParams.get('filter')=='beef'){
+        let param = ".Beef"
+        filter_from_param(param)
+        $('.wil-dropdown #protein-dd li').parents('.wil-dropdown').find('span').text("Beef");
+    }   else if (urlParams.get('filter')=='bacon') {
+        let param = ".Bacon"
+        filter_from_param(param)
+        $('.wil-dropdown #protein-dd li').parents('.wil-dropdown').find('span').text("Bacon");
+    } else if (urlParams.get('filter')=='lamb') {
+        let param = ".Lamb"
+        filter_from_param(param)
+        $('.wil-dropdown #protein-dd li').parents('.wil-dropdown').find('span').text("Lamb");
+    }
+    
+}
+function filter_from_param(param) {
+    let theFilterValue = param
+    $grid.isotope({ filter: theFilterValue });
+    $('ul#protein-dd')
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     // Filter logic
